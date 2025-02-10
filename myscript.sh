5ea88ca3a07b38cbfeb7f2407823f9730d7fbc72
Richmond Baltazar <116050858+Retchizu@users.noreply.github.com>
2025-02-10 06:11:55 +0000

    linux basic script

---
+++ myscript.sh
@@ -0,0 +1,13 @@
+# echo "Hello, Juan!"
+
+
+#read name
+#echo "Your name is $name"
+
+echo "What is your name?"
+read name
+
+echo "What is your favorite food?"
+read food;
+
+echo "Hello $name! I see you love $food!"
